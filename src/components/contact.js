53a946b5e5c85122b9a1ad883a7c7359141f3f40
jj-plane <9252685+jj-plane@users.noreply.github.com>
2020-05-06 11:04:10 -0400

    forgot hidden field that's required by netlify for static sites

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,9 +2,11 @@ import React from "react"
 
 const Contact = () => {
     return(
-        <form name="contact" method="POST" data-netlify="true">
-            <label for="name_input">Name: </label>
-            <input type="text" id="name_input" />
+        <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+            <input type="hidden" data-netlify="true" value="contact" name="contact" />
+            
+            <label for="name">Name: </label>
+            <input type="text" name="name" id="name" />
 
             <button type="submit">Let's Chat</button>
         </form>
